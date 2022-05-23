5a13416da22f1c2d5a76eed2e8cda1512d8082c7
Viet_Faia <nhuviet2012@gmail.com>
2022-05-23 14:52:20 +0700

    Re-update

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -17,8 +17,8 @@ namespace Pet_Store_BE.Models
                 context.Pet.AddRange(
                     new Pet
                     {
-                        Id = "Cat001",
-                        Img = "~/Img/Image/meo-anh-long-ngan/meo-anh-long-ngan-1.jpg",
+                        Id = "C001",
+                        Img = "https://petmaster.vn/petroom/wp-content/uploads/2020/04/meo-anh-1.jpg",
                         Species = "Mèo anh lông ngắn",
                         Purebred = "Thuần chủng",
                         Origin = "Anh Quốc",
@@ -27,9 +27,9 @@ namespace Pet_Store_BE.Models
                     },
                     new Pet
                     {
-                        Id = "Cat002",
-                        Img = "~/Img/Image/meo-anh-long-dai/meo-anh-long-ngan-2.jpg",
-                        Species = "Mèo anh lông ngắn",
+                        Id = "C002",
+                        Img = "https://image-us.eva.vn/upload/4-2020/images/2020-10-12/1602497338-38-thumbnail-width640height480.jpg",
+                        Species = "Mèo anh lông dài",
                         Purebred = "Không thuần chủng",
                         Origin = "Việt Nam",
                         Amount = 12,
@@ -37,11 +37,31 @@ namespace Pet_Store_BE.Models
                     },
                     new Pet
                     {
-                        Id = "Dog003",
-                        Img = "~/Img/Image/cho-poodle.jpg",
-                        Species = "Chó Alaska",
+                        Id = "C003",
+                        Img = "https://traichomeo.com/wp-content/uploads/2022/02/meo-ba-tu-7.jpg",
+                        Species = "Mèo ba tư",
+                        Purebred = "Không thuần chủng",
+                        Origin = "Việt Nam",
+                        Amount = 3,
+                        Price = 250
+                    },
+                    new Pet
+                    {
+                        Id = "D001",
+                        Img = "https://image-us.eva.vn/upload/3-2021/images/2021-09-10/image6-1631238960-626-width650height490.jpg",
+                        Species = "Chó Husky",
+                        Purebred = "Không thuần chủng",
+                        Origin = "Việt Nam",
+                        Amount = 8,
+                        Price = 250
+                    },
+                    new Pet
+                    {
+                        Id = "D002",
+                        Img = "https://petviet.vn/wp-content/uploads/2018/04/hinh-anh-cho-toy-poodle-07-600x521.jpg",
+                        Species = "Chó Poodle",
                         Purebred = "Thuần chủng",
-                        Origin = "Alaska",
+                        Origin = "Pháp",
                         Amount = 12,
                         Price = 1500
                     }
